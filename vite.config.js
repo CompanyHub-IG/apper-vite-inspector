e35edf5ab8efc51920ad8c305a04d1e3c810960a
Ashish <ashish@dreamwares.com>
2025-04-17 16:46:12 +0530

    temp chnages

--- vite.config.js
+++ vite.config.js
@@ -10,30 +10,50 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: 'src/index.js',
+      entry: resolve(__dirname, 'src/index.js'),
       name: 'ApperViteInspector',
       fileName: (format) => `index.${format}.js`,
       formats: ['es', 'umd']
     },
     rollupOptions: {
       // Make sure to externalize deps that shouldn't be bundled
-      external: ['@babel/parser', 'estree-walker', 'magic-string'],
+      external: ['@babel/parser', 'estree-walker', 'magic-string', 'vite'],
       output: {
         // Provide global variables to use in the UMD build
         globals: {
           '@babel/parser': 'BabelParser',
           'estree-walker': 'EstreeWalker',
-          'magic-string': 'MagicString'
+          'magic-string': 'MagicString',
+          'vite': 'Vite'
         }
       }
     },
     target: 'esnext',
     minify: 'terser',
-    sourcemap: true
+    sourcemap: true,
+    cssCodeSplit: false,
+    reportCompressedSize: true,
+    chunkSizeWarningLimit: 1000,
+    emptyOutDir: true,
+    assetsInlineLimit: 4096
   },
   resolve: {
     alias: {
       path: 'path-browserify'
     }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  optimizeDeps: {
+    include: ['@babel/parser', 'estree-walker', 'magic-string']
+  },
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  },
+  commonjsOptions: {
+    include: [/node_modules/],
+    extensions: ['.js', '.cjs']
   }
 });
